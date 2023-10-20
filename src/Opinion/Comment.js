27f8b5aa0a65e1ddb582cb5f37f5ecd9bd3361c0
jang gyu min <gmjang6349@gmail.com>
2023-10-20 18:45:23 +0900

    [규민] feat: 댓글들을 위한 컴포넌트 구현

--- src/Opinion/Comment.js
+++ src/Opinion/Comment.js
@@ -1,8 +1,27 @@
 import React from 'react'
 
-const Comment = () => {
+const Comment = ({ comments }) => {
   return (
-    <div>Comment</div>
+    <>
+      <li>
+        {/* <!-- Avatar --> */}
+        {/* <div className="comment-avatar">
+          <img src="http://i9.photobucket.com/albums/a88/creaticode/avatar_2_zps7de12f8b.jpg" alt="" /></div> */}
+        {/* <!-- Contenedor del Comentario --> */}
+        <div className="comment-box">
+          <div className="comment-head">
+            <h6 className="comment-name">
+              <a href="http://creaticode.com/blog">Lorena Rojero</a></h6>
+            <span>hace 10 minutos</span>
+            <i className="fa fa-reply"></i>
+            <i className="fa fa-heart"></i>
+          </div>
+          <div className="comment-content">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit omnis animi et iure laudantium vitae, praesentium optio, sapiente distinctio illo?
+          </div>
+        </div>
+      </li>
+    </>
   )
 }
 
--- src/Opinion/Opinion.js
+++ src/Opinion/Opinion.js
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Comment from './Comment'
+import axios from 'axios'
 
-const Opinion = () => {
+const Opinion = ({lectureId}) => {
+  const [comments, setcomments] = useState([])
+  const url = process.env.REACT_APP_DEFAULT_URL;
+
+  const getOpioions = async () => {
+    await axios.get(`${url}${lectureId}/opinions`)
+    .then(res => {
+      console.log(res);
+    });
+  }
+  
   return (
-    <div>Opinion</div>
+    <li>
+      <div className="comment-main-level">
+        {/* <!-- Avatar --> */}
+        {/* <div className="comment-avatar">
+          <img src="http://i9.photobucket.com/albums/a88/creaticode/avatar_1_zps8e1c80cd.jpg" alt="" />
+        </div> */}
+        {/* <!-- Contenedor del Comentario --> */}
+        <div className="comment-box">
+          <div className="comment-head">
+            <h6 className="comment-name by-author">
+              <span >Agustin Ortiz</span>
+            </h6>
+            <span>hace 20 minutos</span>
+            <i className="fa fa-reply"></i>
+            <i className="fa fa-heart"></i>
+          </div>
+          <div className="comment-content">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit omnis animi et iure laudantium vitae, praesentium optio, sapiente distinctio illo?
+          </div>
+        </div>
+      </div>
+      {/* <!-- Respuestas de los comentarios --> */}
+      <ul className="comments-list reply-list">
+        <Comment comments={comments}/>
+      </ul>
+    </li>
   )
 }
 
