6da758fe0f46077ec85e85a571b5ac6625b11daf
jang gyu min <gmjang6349@gmail.com>
2023-10-20 18:45:03 +0900

    [규민] feat: 강의 상세 페이지 컴포넌트 구현

--- src/Lecture/LectureDetailPage.js
+++ src/Lecture/LectureDetailPage.js
@@ -1,12 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import LectureDetail from './LectureDetail'
 import OpinionPage from '../Opinion/OpinionPage'
+import { useLocation, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const LectureDetailPage = () => {
+  const location = useLocation();
+  const nav = useNavigate();
+  const [lecture, setLecture] = useState([]);
+  const [lectureId, setlectureId] = useState('');
+  const url = process.env.REACT_APP_DEFAULT_URL;
+
+  useEffect(() => {
+    setlectureId(location.pathname.split('/')[3]);
+    // console.log(lectureId);
+    axios.get(`${url}lecture/${lectureId}`)
+    .then(result => {
+      setLecture(result.data);
+    })
+    .catch((error) => {
+      const message = error.response.data.message;
+      if(message){
+        alert(error.response.data.message);
+        window.location.href="http://localhost:3000/";
+      }
+    });
+  }, [lectureId]);
+
   return (
     <>
-      <LectureDetail/>
-      <OpinionPage/>
+      <LectureDetail lecture={lecture}/>
+      <OpinionPage lectureId={lectureId}/>
     </>
   )
 }
