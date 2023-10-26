eb890f401f88c1516f9058c3891d9f3bc12f0e9d
jgm0327 <gmjang6349@agmail.com>
2023-10-26 16:39:38 +0900

    [규민] fix: 알림과 좋아요 기능 예외 로직 추가

--- src/Lecture/Interest/Interest.js
+++ src/Lecture/Interest/Interest.js
@@ -6,6 +6,7 @@ const Interest = ({lectureId}) => {
 	const url = process.env.REACT_APP_DEFAULT_URL;
 
 	useEffect(() => {
+		if(lectureId === undefined)return;
 		axios.get(`${url}interest/${lectureId}`,
 		{headers: {Authorization: `${localStorage.getItem('token')}`}}
 		)
@@ -14,6 +15,8 @@ const Interest = ({lectureId}) => {
       console.log(isInterested);
 			if(isInterested)setclassname('active')
 			else setclassname('');
+		}).catch((err) => {
+			console.log(err);
 		})
 	}, [lectureId])
 
@@ -22,6 +25,10 @@ const Interest = ({lectureId}) => {
   }, [classname])
 
 	const clickHandler = () => {
+		if(!localStorage.getItem('token')){
+			alert('회원만 등록할 수 있습니다.')
+			return;
+		}
 		axios.post(`${url}user/interest/${lectureId}`,
 		{},
 		{headers: {Authorization: `${localStorage.getItem('token')}`}}
@@ -41,7 +48,7 @@ const Interest = ({lectureId}) => {
 				<path d="M4.03553 1C1.80677 1 0 2.80677 0 5.03553C0 6.10582 0.42517 7.13228 1.18198 7.88909L7.14645 13.8536C7.34171 14.0488 
 						7.65829 14.0488 7.85355 13.8536L13.818 7.88909C14.5748 7.13228 15 6.10582 15 5.03553C15 2.80677 13.1932 1 10.9645 1C9.89418 1 8.86772 1.42517 8.11091 
 						2.18198L7.5 2.79289L6.88909 2.18198C6.13228 1.42517 5.10582 1 4.03553 1Z"
-						strokeWidth="1" strokeLinecap="round" strokeLinejoin="round"/>
+						strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
 				</svg>
 		</button>
 	)
